2d27f61358b39650563287ae181e6fe03042a792
Breno Ribeiro do Val <bval@acme.local>
2020-08-17 12:38:47 -0300

    implementing DepartmentDao

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,74 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+import model.services.DepartmentService;
+
+public class DepartmentListController implements Initializable {
+	
+	private DepartmentService service;
+
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	private Button btNew;
+	
+	private ObservableList<Department> obsList;
+	
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();	
+	}
+
+	private void initializeNodes() {
+		/*
+		 * Command to initialize the behavior of columns and cells of a table
+		 * */
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		/*
+		 * Setting to fit the table as the same size as the main window
+		 * */
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+	}
+}
---
+++ src/model/services/DepartmentService.java
@@ -0,0 +1,16 @@
+package model.services;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentService {
+
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
+	public List<Department> findAll() {
+		return dao.findAll();
+	}
+}
